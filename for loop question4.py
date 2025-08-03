a9b99daea17bf76cb35607672e43add0e1955e4f
sonu kushwaha <sonukuswhaha@gmail.com>
2025-08-03 16:43:51 +0530

    #sum up to n term

---
+++ for loop question4.py
@@ -0,0 +1,11 @@
+#take a number as input and print table
+n=int(input("enter the number"))
+for i in range(1,51):
+     print(n,"*", i,"=", n*i)
+
+
+
+
+# n = int(input("enter the number  for table"))
+# for i in range(2,50):
+#      print(f"{n} * {i} = {n *i}")
