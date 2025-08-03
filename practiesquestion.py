95ba331f54ef5d716511a92255ac139a6357ba62
sonu kushwaha <sonukuswhaha@gmail.com>
2025-08-03 15:04:23 +0530

    Add files via upload

---
+++ practiesquestion.py
@@ -0,0 +1,90 @@
+#Q1:accept  two number and print the greatest between them
+# num1= int(input("enter first number:-"))
+# num2= int(input("enter second  number:-"))
+# if num1 > num2:
+#     print("frst number is greatest them")
+# elif num2 > num1:
+#      print("second number is greatest them")
+# else:
+#      print("both number is greatest them")
+          
+
+
+          #accept the gender from the user as char and print the respective grreting msg
+         # ex: good moring sir(on the baisc of gender)
+# gen = input("please  tell your gender as character (M or F):-")    
+
+# if gen == "M":
+#     print("Good morning SIR ")
+# elif gen =="F":
+#     print("Good morning MAM ")     
+# else:
+#     print ("good moring to you")
+
+
+
+#Q3: accept the interger and check wether it is an even number or odd
+  
+
+
+
+# num = int(input("please tell your number:-"))
+
+# if num%2 == 0:
+#     print("even  number ")
+# else:
+#     print(" odd  number ")
+
+
+#Q4: accept name and age from the user check if the user is valid voter or not
+ 
+# name= input("please tell your name:-")
+# age = int(input("please tell your age:-"))
+# if age >= 18:
+#     print(f" hello {name}you are a valid voter" )
+
+# else:
+#     print(f" hello {name}you are  not a valid  voter" )
+
+#Q5: accept a year and check if it is a leap year or not (goole to find out what is a leap year)
+#400 se completely divide hota ho → ✅ Leap Year
+#Example: 2000 % 400 == 0 → Leap year
+
+
+
+#4 se divide hota ho aur 100 se divide na hota ho → ✅ Leap Year
+#Example: 2024 % 4 == 0 and 2024 % 100 != 0 → Leap year
+
+
+# year = int(input("tell your year:-"))
+
+# if year % 4 ==0 and year % 400 ==0:
+#     print( "its a leap year")
+# elif  year % 100 != 0 and  year % 4 == 0: 
+#      print( "its a leap year")
+# else:
+#      print("its not a leap year")
+
+
+
+
+# t=int(input("please tell the temprature:-"))
+# if t<=0:
+#     print(" freezing cold")
+# elif t>=0 and t<10:
+#     print("very cold")
+# elif t>=10 and  t<20:
+#     print("cold")
+# elif t>=20 and t<30:
+#     print("normal")
+
+# elif t>=30 and t<40:
+#     print("hot")
+
+# else:
+#     print("extremely hot")    
+
+
+
+
+                  # range function#
