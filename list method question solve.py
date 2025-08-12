054c7ae6cb494317a7e27731dab451e7146c195f
sonu kushwaha <sonukuswhaha@gmail.com>
2025-08-12 11:32:20 +0530

    #print positive and neative element of an list
    #print positive and neative element of an list

---
+++ list method question solve.py
@@ -0,0 +1,11 @@
+
+#print positive and neative element of an list
+l=[-45,67,-45 ,98]
+print("positive element are")
+for i in l:
+    if i >= 0:
+        print(l)
+print("neative the number")
+for i in l:
+    print(i)
+""
